5606cb4368cf3cbabc6082f3d87774aa7d3b556e
Bella200625 <bbotellom@unicartagena.edu.co>
2025-09-20 00:04:27 -0500

    se creo la clase producto (entidad) y  la clase DomainException
    Se creó la clase DomainException en el paquete Exception para manejar reglas de negocio.

---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/Entidades/Producto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.Entidades;
+
+/**
+ *
+ * @author mezab
+ */
+public class Producto {
+    
+}
---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/Exceptions/DomainException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.Exceptions;
+
+
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
