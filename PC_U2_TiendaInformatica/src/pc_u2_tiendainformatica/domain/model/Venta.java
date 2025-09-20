00498301a3cf106396c1d3f168d0d8fe04315d6e
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-20 01:16:00 -0500

    Reorganizando

--- PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/model/Venta.java
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/model/Venta.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pc_u2_tiendainformatica.Entidades;
+package pc_u2_tiendainformatica.domain.model;
 
 /**
  *
