369526720c44229cd98e52ad77712ec867238e9d
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-19 23:11:36 -0500

    Clonacion de repositorio y Creacion de proyecto
    Protocolo colaborativo Unidad 2 - Desarrollo de Software, de los estudiantes, Bella Samai Botello Meza y Alvaro jesus perez Menendez.

---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/PC_U2_TiendaInformatica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pc_u2_tiendainformatica;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class PC_U2_TiendaInformatica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
