0e61826241c028e32a45530cc9aba35804c4d368
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-30 17:53:39 -0500

    Cambios minimos, refactor

--- PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/in/RegistrarVentaCaseUse.java
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/in/RegistrarVentaCaseUse.java
@@ -8,7 +8,7 @@ package pc_u2_tiendainformatica.domain.port.in;
  *
  * @author Mitzuke
  */
-public interface RegistrarVenta {   
+public interface RegistrarVentaCaseUse {   
     
     /*Aqui debera ir la swolicitud para realizar una venta Donde contendra los datos necesarios para sus validaconesw
     y el resultado de la venta procesada con la ID de la trnsaccion, Es puerto haria uso del ServicioDeVenta en teoria*/
