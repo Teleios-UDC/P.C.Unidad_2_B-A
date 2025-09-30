c2ad371c6add25317d0ef15c8da447cbb04613b5
Bella200625 <bbotellom@unicartagena.edu.co>
2025-09-30 09:34:07 -0500

    Excepciones
    se han creado  las Excepciones de Dominio claras y específicas.  para cada entidad

---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/exceptions/ServicioTecnicoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.domain.exceptions;
+
+/**
+ *
+ * @author mezab
+ */
+public class ServicioTecnicoException extends RuntimeException {
+    public ServicioTecnicoException(String mensaje) {
+        super(mensaje);
+    }
+}
