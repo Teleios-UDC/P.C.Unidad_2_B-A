9ed3de56844a56daab3f3a5712828b8618f934ba
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-30 10:35:24 -0500

    Creacion de Enum
    Creamos un enum con las categorias que se van a utlizar tienendo en cuenta el CEA.

---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/enums/CategoriaEnum.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.domain.enums;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public enum CategoriaEnum {
+    CPU,
+    IMPRESORA,
+    MONITOR,
+    DISCO_DURO,
+    OTRO;
+    
+    
+}
