db3477c07d9a34af3a44bca017a56cff867c9f50
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-30 14:14:50 -0500

    Puertos de Entrada
    Se Crearon dos puertos de entrada solo con sus firmas.

---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/in/RegistrarVenta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.domain.port.in;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public interface RegistrarVenta {   
+    
+    /*Aqui debera ir la swolicitud para realizar una venta Donde contendra los datos necesarios para sus validaconesw
+    y el resultado de la venta procesada con la ID de la trnsaccion, Es puerto haria uso del ServicioDeVenta en teoria*/
+    
+    //String registrarVenta(Comando RVenta);
+}
---
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/in/SolicitarSoporteTecnico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc_u2_tiendainformatica.domain.port.in;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class SolicitarSoporteTecnico {
+    
+    /*Procesa la solicitud de soporte tecnico, adjuntamos los datos necesarios para registrar el servicio
+    donde nos de el ID Unico del Servicio TEcnico creado
+    
+    si esto no funciona el ServicioTecnicoExcepcion actua para enviar un mensaje de error*/
+    
+    //String SolicitarServicio();
+}
