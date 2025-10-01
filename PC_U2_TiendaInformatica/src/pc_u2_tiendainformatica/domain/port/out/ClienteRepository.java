97b392618219fae32d1a4de7ae75b414784ef0de
Bella200625 <bbotellom@unicartagena.edu.co>
2025-09-30 20:28:01 -0500

    Update ClienteRepository.java

--- PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/out/ClienteRepository.java
+++ PC_U2_TiendaInformatica/src/pc_u2_tiendainformatica/domain/port/out/ClienteRepository.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pc_u2_tiendainformatica.domain.port.out;
-
+import pc_u2_tiendainformatica.domain.model.*;
+import pc_u2_tiendainformatica.domain.exceptions.*;
 /**
  *
  * @author mezab
  */
-public class ClienteRepository {
-    
+
+// Esta interfaz define lo que se puede hacer con los clientes
+// Es un puerto OUT porque el dominio solo declara lo que necesita
+// Aqui decimos que se puede guardar un cliente y buscar un cliente por su id
+public interface ClienteRepository {
+    Cliente salvar(Cliente cliente);
+    Cliente buscarPorId(String idCliente) throws ClienteException;
+            
 }
